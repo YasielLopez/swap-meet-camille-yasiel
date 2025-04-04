2334bcc3b9908ae28a732465ba2904aebc79fcfa
Yasiel <toyasiel@gmail.com>
2025-04-03 22:38:04 -0700

    reworked item code to make it conice.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,25 +2,25 @@ import uuid
 
 class Item:
     def __init__(self, id=None, condition=0):
-        self.id = id if id is not None else uuid.uuid4().int
         self.condition = condition
-
-    def __str__(self):
-        return f"An object of type Item with id {self.id}."
-
+        if id is None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+    
     def get_category(self):
         return "Item"
     
+    def __str__(self):
+        return f"An object of type {self.__class__.__name__} with id {self.id}."
+    
     def condition_description(self):
-        if self.condition == 0.0:
-            return "poor"
-        elif self.condition == 1.0:
-            return "fair"
-        elif self.condition == 2.0:
-            return "acceptable"
-        elif self.condition == 3.0:
-            return "good"
-        elif self.condition == 4.0:
-            return "like new"
-        else:
-            return "brand new"
+        descriptions = [
+            "Poor condition", 
+            "Used condition", 
+            "Fair condition", 
+            "Good condition", 
+            "Excellent condition", 
+            "Mint condition"
+        ]
+        return descriptions[min(self.condition, 5)]
